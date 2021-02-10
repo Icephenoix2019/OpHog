1404cce96d30bc173f91dadf861b38009aa6c5c7
Icephenoix2019 <56941962+Icephenoix2019@users.noreply.github.com>
2021-02-10 09:20:46 -0500

    Update PlayerInventory.js

--- src/js/PlayerInventory.js
+++ src/js/PlayerInventory.js
@@ -39,16 +39,16 @@
         };
 
 		// Add slots for each character class
-		for (var i = 0; i < 3; i++) {
-			var newSlot = new game.PlayerSlot(game.SlotTypes.WAR, this.slotID += 2);
+		for (var i = 0; i < 2; i++) {
+			var newSlot = new game.PlayerSlot(game.SlotTypes.WAR, this.slotID++);
 			this.addSlot(newSlot);
 		};
-		for (var i = 0; i < 3; i++) {
-			var newSlot = new game.PlayerSlot(game.SlotTypes.WIZ, this.slotID += 2);
+		for (var i = 0; i < 2; i++) {
+			var newSlot = new game.PlayerSlot(game.SlotTypes.WIZ, this.slotID++);
 			this.addSlot(newSlot);
 		};
-		for (var i = 0; i < 3; i++) {
-			var newSlot = new game.PlayerSlot(game.SlotTypes.ARCH, this.slotID += 2);
+		for (var i = 0; i < 2; i++) {
+			var newSlot = new game.PlayerSlot(game.SlotTypes.ARCH, this.slotID++);
 			this.addSlot(newSlot);
 		};
 
